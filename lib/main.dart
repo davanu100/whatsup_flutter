bf5ee6dbcb2ecaf39873cd01fa55fc589c9fbc24
ANURAG THAKUR <davanu100@gmail.com>
2020-07-29 03:10:51 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Hikey",
+      home : Scaffold(
+        appBar: AppBar(
+          title: Text('Hikey'),
+        ),
+      ),
+    ),
+  );
+}
